be18de094bdf8290a6788452ba145e97f40dfc27
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-07-15 04:58:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,13 +5,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  num1=10
+  num2=5
+  p num1-num2
 end
 
 def division(num1, num2)
 
 end
-
+  
 def multiplication(num1, num2)
 
 end
